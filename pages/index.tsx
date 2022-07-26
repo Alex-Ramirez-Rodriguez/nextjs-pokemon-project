2c94ee7119fb4bf4bd77d527f8b957ee179d004a
Alex-Ramirez-Rodriguez <pv190115553@vallarta.tecmm.edu.mx>
2022-07-25 22:21:44 -0500

    Inference data structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,18 @@ import { Layout } from '../components/layouts';
 import { pokeApi } from '../api';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
-const HomePage: NextPage = ({ pokemons }: any) => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="Pokemon list">
       <ul>
         {pokemons.map((pokemon: SmallPokemon, idx: any) => (
           <div key={pokemon.url}>
             <li>
-              {idx + 1}.- {pokemon.name}
+              {pokemon.id}.- {pokemon.name}
             </li>
           </div>
         ))}
@@ -23,7 +27,12 @@ const HomePage: NextPage = ({ pokemons }: any) => {
 // No se hace una petición adicional del lado del cliente
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
-  const pokemons = data.results;
+  const pokemons = data.results.map((poke, idx) => ({
+    ...poke,
+    id: idx,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idx}.svg`,
+  }));
+  console.log('🚀 ~ file: index.tsx ~ line 35 ~ pokemons ~ pokemons', pokemons);
 
   return {
     props: {
